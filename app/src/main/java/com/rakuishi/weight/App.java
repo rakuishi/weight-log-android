782df871bd86f28116108ad735de112ece9f363f
rakuishi <rakuishi@gmail.com>
2017-04-21 07:03:31 +0900

    Login with google-fit-account

---
+++ app/src/main/java/com/rakuishi/weight/App.java
@@ -0,0 +1,17 @@
+package com.rakuishi.weight;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
--- app/src/main/java/com/rakuishi/weight/MainActivity.java
+++ app/src/main/java/com/rakuishi/weight/MainActivity.java
@@ -1,13 +1,125 @@
 package com.rakuishi.weight;
 
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int REQUEST_OAUTH = 1;
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
+    private GoogleApiClient client = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
+        }
+
+        buildFitnessClient();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        client.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (client.isConnected()) {
+            client.disconnect();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_OAUTH) {
+            authInProgress = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!client.isConnecting() && !client.isConnected()) {
+                    client.connect();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(AUTH_PENDING, authInProgress);
+    }
+
+    // region GoogleApiClient.ConnectionCallbacks
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Timber.d("onConnected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Timber.d("onConnectionSuspended");
+    }
+
+    // endregion
+
+    // region GoogleApiClient.OnConnectionFailedListener
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Timber.d("onConnectionFailed: " + connectionResult.toString());
+        if (!connectionResult.hasResolution()) {
+            // Show the localized error dialog
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(),
+                    MainActivity.this, 0).show();
+            return;
+        }
+
+        if (!authInProgress) {
+            try {
+                Timber.d("Attempting to resolve failed connection");
+                authInProgress = true;
+                connectionResult.startResolutionForResult(MainActivity.this, REQUEST_OAUTH);
+            } catch (IntentSender.SendIntentException e) {
+                Timber.d(e.getMessage());
+            }
+        }
+    }
+
+    // endregion
+
+    // region private methods
+
+    private void buildFitnessClient() {
+        // https://developers.google.com/android/reference/com/google/android/gms/fitness/HistoryApi
+        // https://developers.google.com/android/reference/com/google/android/gms/common/Scopes#FITNESS_BODY_READ_WRITE
+
+        client = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_BODY_READ_WRITE))
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    // endregion
 }
