eddfd010c54d1cd21f69bad1d8a766972e0f4033
rakuishi <rakuishi@gmail.com>
2017-04-21 21:08:58 +0900

    Load weight data

--- app/src/main/java/com/rakuishi/weight/MainActivity.java
+++ app/src/main/java/com/rakuishi/weight/MainActivity.java
@@ -2,10 +2,10 @@ package com.rakuishi.weight;
 
 import android.content.Intent;
 import android.content.IntentSender;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -13,8 +13,28 @@ import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResult;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
+
+import static java.text.DateFormat.getDateInstance;
+import static java.text.DateFormat.getTimeInstance;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
@@ -22,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private static final String AUTH_PENDING = "auth_state_pending";
     private boolean authInProgress = false;
     private GoogleApiClient client = null;
+    private CompositeDisposable compositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +52,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         if (savedInstanceState != null) {
             authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
         }
+
+        compositeDisposable = new CompositeDisposable();
 
         buildFitnessClient();
     }
@@ -47,6 +70,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         if (client.isConnected()) {
             client.disconnect();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.dispose();
+        super.onDestroy();
     }
 
     @Override
@@ -73,6 +102,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         Timber.d("onConnected");
+        loadWeightData();
     }
 
     @Override
@@ -119,6 +149,53 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+    }
+
+    private void loadWeightData() {
+        Observable<DataReadResult> observable = Observable.create(e -> {
+            e.onNext(Fitness.HistoryApi.readData(client, queryWeightData()).await(1, TimeUnit.MINUTES));
+            e.onComplete();
+        });
+        Disposable disposable = observable
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(dataReadResult -> {
+                    if (dataReadResult != null) {
+                        DateFormat dateFormat = getDateInstance();
+                        for (DataSet dataSet : dataReadResult.getDataSets()) {
+                            for (DataPoint dataPoint : dataSet.getDataPoints()) {
+                                Timber.d("Data Point:");
+                                Timber.d("\tType: " + dataPoint.getDataType().getName());
+                                Timber.d("\tStart: " + dateFormat.format(dataPoint.getStartTime(TimeUnit.MILLISECONDS)));
+                                Timber.d("\tEnd: " + dateFormat.format(dataPoint.getEndTime(TimeUnit.MILLISECONDS)));
+                                for (Field field : dataPoint.getDataType().getFields()) {
+                                    Timber.d("\tFiled: " + field.getName() + ", Value: " + dataPoint.getValue(field));
+                                }
+                            }
+                        }
+                    }
+                });
+        compositeDisposable.add(disposable);
+    }
+
+    private DataReadRequest queryWeightData() {
+        Calendar calendar = Calendar.getInstance();
+        Date now = new Date();
+        calendar.setTime(now);
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.WEEK_OF_YEAR, -4);
+        long startTime = calendar.getTimeInMillis();
+
+        DateFormat dateFormat = getDateInstance();
+        Timber.d("Range Start: " + dateFormat.format(startTime));
+        Timber.d("Range End: " + dateFormat.format(endTime));
+
+        DataReadRequest request = new DataReadRequest.Builder()
+                .read(DataType.TYPE_WEIGHT)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+        return request;
     }
 
     // endregion
