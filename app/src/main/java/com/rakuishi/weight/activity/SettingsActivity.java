c366fc4457c12d61341c1529f025c797aa18b1c8
rakuishi <rakuishi@gmail.com>
2017-05-03 14:21:10 +0900

    Update SettingsActivity.java #9

--- app/src/main/java/com/rakuishi/weight/activity/SettingsActivity.java
+++ app/src/main/java/com/rakuishi/weight/activity/SettingsActivity.java
@@ -3,12 +3,15 @@ package com.rakuishi.weight.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.rakuishi.weight.BuildConfig;
 import com.rakuishi.weight.R;
 import com.rakuishi.weight.databinding.ActivitySettingsBinding;
 import com.rakuishi.weight.repo.FitnessClient;
@@ -27,17 +30,28 @@ public class SettingsActivity extends AppCompatActivity implements FitnessClient
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_settings);
-        binding.bombButton.setOnClickListener(v -> {
-            if (client.isConnected()) {
-                client.clearDefaultAccountAndReconnect();
-            }
-        });
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.settings);
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        binding.bombTextView.setOnClickListener(v -> {
+            if (client.isConnected()) {
+                client.clearDefaultAccountAndReconnect();
+            }
+        });
+
+        String version = String.format(getString(R.string.version_format), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
+        binding.versionTextView.setText(version);
+
+        binding.developerTextView.setOnClickListener(v -> {
+            Uri uri = Uri.parse(getString(R.string.developer_website));
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.launchUrl(SettingsActivity.this, uri);
+        });
 
         client = new FitnessClient(this, this);
     }
